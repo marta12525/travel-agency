258bca8f79f22482f3a2b889231f261de47499c4
Marta Bednarz <martab2525@gmail.com>
2020-06-16 18:05:04 +0200

    add features to order option, add text and date

--- src/components/features/OrderForm/OrderFormContainer.js
+++ src/components/features/OrderForm/OrderFormContainer.js
@@ -1,11 +1,13 @@
 import {connect} from 'react-redux';
-import {getOrderOptions} from '../../../redux/orderRedux';
 import OrderForm from './OrderForm';
-
+import {getOrderOptions, setOrderOption} from '../../../redux/orderRedux';
 
 const mapStateToProps = state => ({
   options: getOrderOptions(state),
 });
 
+const mapDispatchToProps = dispatch => ({
+  setOrderOption: (option) => dispatch(setOrderOption(option)),
+});
 
-export default connect(mapStateToProps)(OrderForm);
+export default connect(mapStateToProps, mapDispatchToProps)(OrderForm); 
